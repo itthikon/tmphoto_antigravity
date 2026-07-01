c17bc0fd43bb7c16dc28ed9c52ac07613f54989c
Itthikon <zero-no-connect@hotmail.com>
2026-07-01 23:44:34 +0700

    Load dotenv with absolute path in database.js

--- src/db/database.js
+++ src/db/database.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const dbType = process.env.DB_TYPE || 'sqlite';
 
